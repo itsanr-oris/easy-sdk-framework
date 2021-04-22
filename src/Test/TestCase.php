78230525d57d94be256b6e895f8f505afd1b38b1
F.oris <oris@itsanr.com>
2021-04-22 22:24:26 +0800

    feat: 优化测试代码逻辑

---
+++ src/Test/TestCase.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Foris\Easy\Sdk\Test;
+
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\StreamOutput;
+
+/**
+ * Class TestCase
+ */
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Application instance.
+     *
+     * @var \Foris\Easy\Sdk\Application
+     */
+    protected $app;
+
+    /**
+     * Artisan command application instance.
+     *
+     * @var \Foris\Easy\Sdk\Console\Application
+     */
+    protected $artisan;
+
+    /**
+     * ConsoleOutput instance.
+     *
+     * @var ConsoleOutput
+     */
+    protected $output;
+
+    /**
+     * Set up test environment
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->app = $this->createApplication();
+        $this->artisan = $this->createArtisan();
+    }
+
+    /**
+     * Create sdk application instance.
+     *
+     * @return \Foris\Easy\Sdk\Application
+     */
+    abstract protected function createApplication();
+
+    /**
+     * Create artisan command application instance.
+     *
+     * @return \Foris\Easy\Sdk\Console\Application
+     */
+    abstract protected function createArtisan();
+
+    /**
+     * Gets the application instance
+     *
+     * @return \Foris\Easy\Sdk\Application
+     */
+    protected function app()
+    {
+        return $this->app;
+    }
+
+    /**
+     * Gets the artisan command application instance.
+     *
+     * @return \Foris\Easy\Sdk\Console\Application
+     */
+    protected function artisan()
+    {
+        return $this->artisan;
+    }
+
+    /**
+     * Run an Artisan console command by name.
+     *
+     * @param       $command
+     * @param array $parameters
+     * @return int
+     *
+     * @throws \Exception
+     */
+    public function command($command, $parameters = [])
+    {
+        return $this->artisan()->call($command, $parameters, $this->initOutput());
+    }
+
+    /**
+     * Initializes the output property.
+     *
+     * @param array $options
+     * @return \Symfony\Component\Console\Output\OutputInterface
+     */
+    protected function initOutput($options = [])
+    {
+        $this->output = new StreamOutput(fopen('php://memory', 'w', false));
+        if (isset($options['decorated'])) {
+            $this->output->setDecorated($options['decorated']);
+        }
+        if (isset($options['verbosity'])) {
+            $this->output->setVerbosity($options['verbosity']);
+        }
+        return $this->output;
+    }
+
+    /**
+     * Gets the display returned by the last execution of the command or application.
+     *
+     * @param bool $normalize
+     * @return string The display
+     */
+    public function getDisplay($normalize = false)
+    {
+        if (null === $this->output) {
+            throw new \RuntimeException('Output not initialized, did you execute the command before requesting the display?');
+        }
+
+        rewind($this->output->getStream());
+
+        $display = stream_get_contents($this->output->getStream());
+
+        if ($normalize) {
+            $display = str_replace(\PHP_EOL, "\n", $display);
+        }
+
+        return $display;
+    }
+
+    /**
+     * Assert a given string is a sub-string of another string.
+     *
+     * @param string $needle
+     * @param string $haystack
+     * @param string $message
+     */
+    protected function assertHasSubString($needle, $haystack, $message = '')
+    {
+        if (method_exists($this, 'assertStringContainsString')) {
+            $this->assertStringContainsString($needle, $haystack, $message);
+            return ;
+        }
+
+        $this->assertTrue(mb_strpos($haystack, $needle) !== false);
+    }
+
+    /**
+     * Assert a given string is a sub-string of another string.
+     *
+     * @param string $needle
+     * @param string $haystack
+     * @param string $message
+     */
+    protected function assertHasSubStringIgnoringCase($needle, $haystack, $message = '')
+    {
+        if (method_exists($this, 'assertStringContainsStringIgnoringCase')) {
+            $this->assertStringContainsStringIgnoringCase($needle, $haystack, $message);
+            return ;
+        }
+
+        $this->assertTrue(mb_stripos($haystack, $needle) !== false);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,17 +8,8 @@ use org\bovigo\vfs\vfsStream;
 /**
  * Class TestCase
  */
-class TestCase extends \PHPUnit\Framework\TestCase
+class TestCase extends \Foris\Easy\Sdk\Test\TestCase
 {
-    use ArtisanTestSuite;
-
-    /**
-     * Application instance.
-     *
-     * @var Application
-     */
-    protected $app;
-
     /**
      * vfsStreamDirectory instance
      *
@@ -31,20 +22,29 @@ class TestCase extends \PHPUnit\Framework\TestCase
      */
     protected function setUp()
     {
-        parent::setUp();
-
         $this->initVfs();
-        $this->app = new Application();
+        parent::setUp();
     }
 
     /**
-     * Gets application instance
+     * Create sdk application instance.
      *
-     * @return Application
+     * @return Application|\Foris\Easy\Sdk\Application
      */
-    protected function app()
+    protected function createApplication()
     {
-        return $this->app;
+        return new Application();
+    }
+
+    /**
+     * Create artisan command application instance.
+     *
+     * @return \Foris\Demo\Sdk\Console\Application|\Foris\Easy\Sdk\Console\Application
+     * @throws \ReflectionException
+     */
+    protected function createArtisan()
+    {
+        return new \Foris\Demo\Sdk\Console\Application($this->app());
     }
 
     /**
@@ -71,39 +71,5 @@ class TestCase extends \PHPUnit\Framework\TestCase
         }
 
         return $this->vfs;
-    }
-
-    /**
-     * Assert a given string is a sub-string of another string.
-     *
-     * @param string $needle
-     * @param string $haystack
-     * @param string $message
-     */
-    protected function assertHasSubString($needle, $haystack, $message = '')
-    {
-        if (method_exists($this, 'assertStringContainsString')) {
-            $this->assertStringContainsString($needle, $haystack, $message);
-            return ;
-        }
-
-        $this->assertTrue(mb_strpos($haystack, $needle) !== false);
-    }
-
-    /**
-     * Assert a given string is a sub-string of another string.
-     *
-     * @param string $needle
-     * @param string $haystack
-     * @param string $message
-     */
-    protected function assertHasSubStringIgnoringCase($needle, $haystack, $message = '')
-    {
-        if (method_exists($this, 'assertStringContainsStringIgnoringCase')) {
-            $this->assertStringContainsStringIgnoringCase($needle, $haystack, $message);
-            return ;
-        }
-
-        $this->assertTrue(mb_stripos($haystack, $needle) !== false);
     }
 }
